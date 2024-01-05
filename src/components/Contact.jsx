a7482044f833522a9224a93de376c392767cb558
Patrick <patricklee260@gmail.com>
2024-01-04 17:39:25 -0800

    added links to various projects I've done that I can't fit in my resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or send me an email - email</p>
+                <p className='text-gray-300 py-4'>Submit the form below or send me an email - patricklee260@gmail.com</p>
             </div>
             <input className='bg-white p-2' type='text' placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-white' type='email' placeholder='Email' name='email' />
---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import KSCU from "../assets/KSCU.png"
+import Drem from "../assets/Drem.png"
+import VaxTrax from "../assets/VaxTrax.png"
+import RayTracer from "../assets/RayTracer.png"
+import Opal from "../assets/Opal.png"
+import CHEF from "../assets/CHEF.png"
+
+export const data = [
+    {
+        id:1,
+        name:"KSCU Radio Station",
+        image: KSCU,
+        github:"https://github.com/KSCU-radio/kscu-site",
+        demo:"https://kscu.org/credits/",
+    },
+    {
+        id:2,
+        name:"Drem Landing Page",
+        image: Drem,
+        github:"https://github.com/Drem-Labs/drem-landing-ui",
+        demo:"https://www.drem.xyz/",
+    },
+    {
+        id:3,
+        name:"VaxTrax (Hack 4 Humanity 2021 1st Place)",
+        image: VaxTrax,
+        github:"https://github.com/PatrickStarLee/vax-trax",
+        demo:"https://devpost.com/software/vaxtrax",
+    },
+    {
+        id:4,
+        name:"Graphics Ray Tracer",
+        image: RayTracer,
+        github:"https://github.com/kristinayin/Raytracer",
+        demo:"https://media.licdn.com/dms/document/media/C562DAQG-fYpMV4C88w/profile-treasury-document-pdf-analyzed/0/1639681365894?e=1705536000&v=beta&t=jQ85CCn96rf_Bld4yqdzT1dIvhqFHR5dOCYw-Gzatuk",
+    },
+    {
+        id:5,
+        name:"Opal Mobile App",
+        image: Opal,
+        github:"",
+        demo:"https://media.licdn.com/dms/document/media/C562DAQHVdx_MPFt02A/profile-treasury-document-pdf-analyzed/0/1608656484288?e=1705536000&v=beta&t=_kFF-IoYaGSpCedmdRsVVdOxFpl9npuGvsJFP1Esuew",
+    },
+    {
+        id:6,
+        name:"CHEF (Hack 4 Humanity 2020)",
+        image: CHEF,
+        github:"https://github.com/vedyakonda/CHEF",
+        demo:"https://devpost.com/software/children-education-foundation-chef#updates",
+    },
+]
